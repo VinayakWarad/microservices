f232415ba230326ef03b3d809f3cfdaf788d6d70
vinayak.warad <vinayr.warad@gmail.com>
2025-01-30 08:25:58 +0530

    My Test comment

--- section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -35,7 +35,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		)
 )
 public class AccountsApplication {
-	//MyTest comment here
+	//MyTest comment
 
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
